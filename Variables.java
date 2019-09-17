bb937167a7c254c9eff8e46cc38195e9b29c6694
Thomas B <32444844+tjbrett03@users.noreply.github.com>
2019-09-17 14:39:19 -0500

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,56 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 420;
+		int intOne = 42069;
+		long longOne = 157290235;
+		
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
